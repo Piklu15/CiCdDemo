589126927b77cca71553dcfb4366b14b2b0c574b
piklu.majumder <piklu.majumder@bjitgroup.com>
2024-08-28 18:09:06 +0600

    [CICD] new test file added

--- src/main/java/com/bjitacademy/cicd/CicdDemo/CicdDemoApplication.java
+++ src/main/java/com/bjitacademy/cicd/CicdDemo/CicdDemoApplication.java
@@ -1,13 +1,26 @@
 package com.bjitacademy.cicd.CicdDemo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CicdDemoApplication {
 
+	public static final Logger logger = LoggerFactory.getLogger(CicdDemoApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(CicdDemoApplication.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner initialSetup() {
+		return args -> {
+			logger.info("Initial setup is done");
+		};
 	}
 
 }
--- src/test/java/com/bjitacademy/cicd/CicdDemo/CicdDemoApplicationTests.java
+++ src/test/java/com/bjitacademy/cicd/CicdDemo/CicdDemoApplicationTests.java
@@ -1,13 +1,26 @@
 package com.bjitacademy.cicd.CicdDemo;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
 class CicdDemoApplicationTests {
+
+	public static final Logger logger = LoggerFactory.getLogger(CicdDemoApplication.class);
+
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	public void test() {
+		logger.info("Welcome to CICD test");
+        assertTrue(true);
+	}
 }
